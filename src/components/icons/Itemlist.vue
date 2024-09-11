f03da96f324e84e5362711f876abf8ccd6cf0bbe
Feltimos <johnfeltimosmichael@gmail.com>
2024-09-11 15:18:14 +0800

    Prelim Examination

---
+++ src/components/icons/Itemlist.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {computed} from 'vue'
+const products = [
+  {
+    name: 'Burger',
+    price: 20
+  },
+  {
+    name: 'Soda',
+    price: 25
+  },
+  {
+    name: 'Water',
+    price: 20
+  }
+]
+
+const totalPrice = computed(() => {
+  return products.reduce((sum, item) => item.price + sum, 0)
+})
+</script>
+
+<template>
+  <div>
+    <h1>All Products Buyed</h1>
+    <ul>
+      <li v-for="(item, index) in products" :key="index">{{ item.name }} - P {{ item.price }}</li>
+    </ul>
+    <p>Total Price: {{ totalPrice }}</p>
+  </div>
+</template>
